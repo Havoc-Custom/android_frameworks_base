a66fa5c693ff5b73a8cd5ef8698d8721e6072d91
Devin Moore <devinmoore@google.com>
2022-04-20 22:55:34 +0000

    AudioService: catch RuntimeException for IServiceManager.getService
    IServiceManager.getService() can throw a RuntimeException and
    AudioService can handle that the same way it currently handles the
    RemoteException from the same method.
    
    Bug: 218588089
    Test: Boot cuttlefish with a fake local ServiceManager with no hwservicemanager
    installed.
    
    Change-Id: I5af8a17312a6321fa6b6c20d03392e553d091f8f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -12345,7 +12345,7 @@ public class AudioService extends IAudioService.Stub
                 }
             }
             return pids;
-        } catch (RemoteException e) {
+        } catch (RemoteException | RuntimeException e) {
             return new HashSet<Integer>();
         }
     }
